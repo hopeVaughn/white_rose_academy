f74bb492b3a63e125bcbfb009a9e441a38eab914
hopeVaughn <hopevaughnwarren@gmail.com>
2023-11-28 11:54:10 -0800

    add forms for create page

--- src/components/CreateCourseForm.tsx
+++ src/components/CreateCourseForm.tsx
@@ -1,11 +1,108 @@
 'use client';
 import React from 'react';
-
+import { Form, FormControl, FormField, FormItem, FormLabel } from './ui/form';
+import { z } from 'zod';
+import { createChapterSchema } from '@/validators/course';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Input } from './ui/input';
+import { Separator } from './ui/separator';
+import { Button } from './ui/button';
+import { Plus, Trash } from 'lucide-react';
 type Props = {};
 
+type Input = z.infer<typeof createChapterSchema>;
+
 const CreateCourseForm = (props: Props) => {
+  const form = useForm<Input>({
+    resolver: zodResolver(createChapterSchema),
+    defaultValues: {
+      title: '',
+      units: ['', '', ''],
+    },
+  });
+
+  function onSubmit(data: Input) {
+    console.log(data);
+
+  }
+
+  form.watch();
+
   return (
-    <div className='w-full'></div>
+    <div className='w-full'>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className='w-full mt-4'>
+          <FormField
+            control={form.control}
+            name='title'
+            render={({ field }) => {
+              return (
+                <FormItem className='flex flex-col items-start w-full sm:items-center sm:flex-row'>
+                  <FormLabel className='flex-[1] text-xl'>
+                    Subject
+                  </FormLabel>
+                  <FormControl className='flex-[6]'>
+                    <Input
+                      placeholder='What is the subject you want to learn?'
+                      {...field}
+                    />
+                  </FormControl>
+                </FormItem>
+              );
+            }}
+          />
+          {form.watch('units').map((_, index) => {
+            return (
+              <FormField
+                key={index}
+                control={form.control}
+                name={`units.${index}`}
+                render={({ field }) => {
+                  return (
+                    <FormItem className="flex flex-col items-start w-full sm:items-center sm:flex-row">
+                      <FormLabel className="flex-[1] text-xl">
+                        Unit {index + 1}
+                      </FormLabel>
+                      <FormControl className='flex-[6]'>
+                        <Input
+                          placeholder='Enter the specifics of the subject you want to learn'
+                          {...field}
+                        />
+                      </FormControl>
+                    </FormItem>
+                  );
+                }}
+              />
+            );
+          })}
+          <div className="flex items-center justify-center mt-4">
+            <Separator className='flex-[1]' />
+            <div className="mx-4">
+              <Button
+                type="button"
+                variant='secondary'
+                className='font-semibold'
+                onClick={() => {
+                  form.setValue('units', [...form.watch('units')]);
+                }}
+              >Add Module
+                <Plus className="w-4 h-4 ml-2 text-green-500" />
+              </Button>
+              <Button
+                type="button"
+                variant='secondary'
+                className='font-semibold ml-2'
+
+              >Remove Module
+                <Trash className="w-4 h-4 ml-2 text-red-500" />
+              </Button>
+            </div>
+            <Separator className='flex-[1]' />
+          </div>
+        </form>
+      </Form>
+    </div>
   );
 };
 

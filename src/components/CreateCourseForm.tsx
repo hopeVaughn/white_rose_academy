7be350cac33a16b3ef614cf2f1dbb468ffcccef6
hopeVaughn <hopevaughnwarren@gmail.com>
2023-11-29 12:00:12 -0800

    add framer for createcourse page

--- src/components/CreateCourseForm.tsx
+++ src/components/CreateCourseForm.tsx
@@ -9,6 +9,7 @@ import { Input } from './ui/input';
 import { Separator } from './ui/separator';
 import { Button } from './ui/button';
 import { Plus, Trash } from 'lucide-react';
+import { motion, AnimatePresence } from 'framer-motion';
 type Props = {};
 
 type Input = z.infer<typeof createChapterSchema>;
@@ -53,30 +54,44 @@ const CreateCourseForm = (props: Props) => {
               );
             }}
           />
-          {form.watch('units').map((_, index) => {
-            return (
-              <FormField
-                key={index}
-                control={form.control}
-                name={`units.${index}`}
-                render={({ field }) => {
-                  return (
-                    <FormItem className="flex flex-col items-start w-full sm:items-center sm:flex-row">
-                      <FormLabel className="flex-[1] text-xl">
-                        Unit {index + 1}
-                      </FormLabel>
-                      <FormControl className='flex-[6]'>
-                        <Input
-                          placeholder='Enter the specifics of the subject you want to learn'
-                          {...field}
-                        />
-                      </FormControl>
-                    </FormItem>
-                  );
-                }}
-              />
-            );
-          })}
+          <AnimatePresence>
+            {form.watch('units').map((_, index) => {
+              return (
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, height: 0 }}
+                  animate={{ opacity: 1, height: 'auto' }}
+                  exit={{ opacity: 0, height: 0 }}
+                  transition={{
+                    opacity: { duration: 0.2 },
+                    height: { duration: 0.2 },
+                  }}
+                >
+                  <FormField
+                    key={index}
+                    control={form.control}
+                    name={`units.${index}`}
+                    render={({ field }) => {
+                      return (
+                        <FormItem className="flex flex-col items-start w-full sm:items-center sm:flex-row">
+                          <FormLabel className="flex-[1] text-xl">
+                            Unit {index + 1}
+                          </FormLabel>
+                          <FormControl className='flex-[6]'>
+                            <Input
+                              placeholder='Enter the specifics of the subject you want to learn'
+                              {...field}
+                            />
+                          </FormControl>
+                        </FormItem>
+                      );
+                    }}
+                  />
+                </motion.div>
+              );
+            })}
+          </AnimatePresence>
+
           <div className="flex items-center justify-center mt-4">
             <Separator className='flex-[1]' />
             <div className="mx-4">
@@ -103,6 +118,13 @@ const CreateCourseForm = (props: Props) => {
             </div>
             <Separator className='flex-[1]' />
           </div>
+          <Button
+            type="submit"
+            className='w-full mt-6'
+            size='lg'
+          >
+            Lets Go!
+          </Button>
         </form>
       </Form>
     </div>

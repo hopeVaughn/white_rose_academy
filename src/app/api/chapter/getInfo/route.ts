2000fe44b2f293fe6996769c84e6bc20e2afa3ab
hopeVaughn <hopevaughnwarren@gmail.com>
2023-12-02 10:41:42 -0800

    update /getInfo and youtube functions to update db properly

--- src/app/api/chapter/getInfo/route.ts
+++ src/app/api/chapter/getInfo/route.ts
@@ -1,12 +1,7 @@
 // /api/chapter/getInto
-
 import { prisma } from "@/lib/db";
 import { strict_output } from "@/lib/gpt";
-import {
-  getQuestionsFromTranscript,
-  getTranscript,
-  searchYoutube,
-} from "@/lib/youtube";
+import { getQuestionsFromTranscript, getTranscript, searchYoutube } from "@/lib/youtube";
 import { NextResponse } from "next/server";
 import { z } from "zod";
 
@@ -19,76 +14,49 @@ export async function POST(req: Request, res: Response) {
     const body = await req.json();
     const { chapterId } = bodyParser.parse(body);
     const chapter = await prisma.chapter.findUnique({
-      where: {
-        id: chapterId,
-      },
+      where: { id: chapterId },
     });
     if (!chapter) {
-      return NextResponse.json(
-        {
-          success: false,
-          error: "Chapter not found",
-        },
-        { status: 404 }
-      );
+      return NextResponse.json({ success: false, error: "Chapter not found" }, { status: 404 });
     }
+
     const videoId = await searchYoutube(chapter.youtubeSearchQuery);
     let transcript = await getTranscript(videoId);
     let maxLength = 2000;
     transcript = transcript.split(" ").slice(0, maxLength).join(" ");
 
-    const systemPromptForSummary = `You are an AI capable of summarizing a YouTube transcript in JSON format. Summarize the following transcript in 250 words or less, avoiding mention of sponsors or unrelated topics.`;
-    // Get summary from AI
-    const summary = await strict_output(systemPromptForSummary, transcript);
+    const systemPromptForSummary = `You are an AI capable of summarizing a YouTube transcript in JSON format. Provide a concise summary of the following transcript in 250 words or less. Example Format: {"summary": "Concise summary of the video content."}`;
 
-    const questions = await getQuestionsFromTranscript(
-      transcript,
-      chapter.name
-    );
-    console.log("QUESTIONS", questions);
+    // Get summary from AI
+    const summaryResponse = await strict_output(systemPromptForSummary, transcript);
+    const summary = summaryResponse && summaryResponse[0] ? summaryResponse[0].summary || "" : ""; // Ensure we get the summary string
+
+    const questions = await getQuestionsFromTranscript(transcript, chapter.name);
 
     await prisma.chapter.update({
       where: { id: chapterId },
-      data: {
-        videoId: videoId,
-        summary: summary,
-      },
+      data: { videoId, summary },
     });
 
-    await prisma.question.createMany({
-      data: questions.map((question) => {
-        let options = [
-          question.answer,
-          question.option1,
-          question.option2,
-          question.option3,
-        ];
-        options = options.sort(() => Math.random() - 0.5);
-        return {
+    if (questions.length > 0) {
+      await prisma.question.createMany({
+        data: questions.map((question) => ({
+          chapterId,
           question: question.question,
           answer: question.answer,
-          options: JSON.stringify(options),
-          chapterId: chapterId,
-        };
-      }),
-    });
+          options: JSON.stringify([question.answer, question.option1, question.option2, question.option3].sort(() => Math.random() - 0.5)),
+        })),
+      });
+    }
 
     return NextResponse.json({ success: true });
   } catch (error) {
-    if (error instanceof z.ZodError) {
-      return NextResponse.json(
-        {
-          success: false,
-          error: "Invalid body",
-        },
-        { status: 400 }
-      );
-    } else {
-      console.error("Error in /api/chapter/getInfo:", error);
-      return NextResponse.json({
-        success: false,
-        error: (error as Error).message || "An unknown error occurred",
-      }, { status: 500 });
+    let errorMessage = "An unknown error occurred";
+    if (error instanceof Error) {
+      errorMessage = error.message;
     }
+    console.error("Error in /api/chapter/getInfo:", error);
+    return NextResponse.json({ success: false, error: errorMessage }, { status: 500 });
   }
 }
+
--- src/lib/youtube.ts
+++ src/lib/youtube.ts
@@ -49,7 +49,21 @@ export async function getQuestionsFromTranscript(
   transcript: string,
   course_name: string
 ): Promise<Question[]> {
-  const systemPrompt = `You are a helpful AI assistant capable of generating MCQ questions and answers in JSON format. Each answer should not be more than 15 words. Generate questions about ${course_name} based on the following transcript:`;
+  const systemPrompt = `You are a helpful AI assistant capable of generating MCQ questions and answers in JSON format, suitable for storing in a database. Each question should include a question text, a correct answer, and three incorrect options. Each answer should not be more than 15 words. Generate questions about ${course_name} based on the following transcript:
+
+  Example Format:
+  
+    {
+      "question": "What is the main technique to improve in swimming?",
+      "answer": "Consistent practice and technique refinement",
+      "option1": "Only swimming in competitions",
+      "option2": "Focusing solely on speed",
+      "option3": "Consistent practice and technique refinement",
+      "chapterId": "unique_chapter_id"  // To be filled with the actual chapter ID
+    },
+    // More questions here...
+  `;
+
 
   const userPrompt = new Array(5).fill(
     `Generate an appropriate and challenging MCQ question about ${course_name} with context of the following transcript: ${transcript}`
@@ -57,24 +71,11 @@ export async function getQuestionsFromTranscript(
 
   // Call strict_output and expect a JSON array or object of questions
   const response = await strict_output(systemPrompt, userPrompt);
-  console.log("RESPONSE in YOUTUBE util", response);
+  console.log("QUESTIONS RESPONSE in YOUTUBE /lib", response);
 
   // If response is an array, return it directly
   if (Array.isArray(response)) {
     return response;
   }
-
-  // If response is an object, extract questions into an array
-  // if (response && typeof response === 'object') {
-  //   let questions = [];
-  //   for (let key in response) {
-  //     if (response.hasOwnProperty(key) && typeof response[key] === 'object' && response[key] !== null) {
-  //       questions.push(response[key]);
-  //     }
-  //   }
-  //   return questions;
-  // }
-
-  // If response is neither an array nor a suitable object, return an empty array
   return [];
 }

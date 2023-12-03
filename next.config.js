ec7bc8871697825c96a32ecb2d7dda296235dc62
hopeVaughn <hopevaughnwarren@gmail.com>
2023-12-03 11:37:00 -0800

    update config and gallery page

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        pathname: '**'
-      },
-    ],
+    domains: ["lh3.googleusercontent.com", "s3.us-west-2.amazonaws.com"],
   },
   eslint: {
     ignoreDuringBuilds: true,

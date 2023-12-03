bd2ae0e55d2b92eb5fa728a553257311e9f372f5
hopeVaughn <hopevaughnwarren@gmail.com>
2023-12-03 12:02:38 -0800

    add icon for sub action button

--- src/components/SubscriptionAction.tsx
+++ src/components/SubscriptionAction.tsx
@@ -1,6 +1,9 @@
 'use client';
 import { useSession } from 'next-auth/react';
 import React from 'react';
+import { Progress } from './ui/progress';
+import { Button } from './ui/button';
+import { ZapIcon } from 'lucide-react';
 
 type Props = {};
 
@@ -9,6 +12,13 @@ const SubscriptionAction = (props: Props) => {
   return (
     <div className='flex flex-col items-center w-1/2 p-4 mx-auto mt-4 rounded-md bg-secondary'>
       {data?.user.credits} / 5 Free Courses
+      <Progress
+        className='mt-2'
+        value={data?.user.credits ? (data.user.credits / 5) * 100 : 0} />
+      <Button className='flex mt-3 font-bold text-white transition bg-gradient-to-tr from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600'>
+        Upgrade
+        <ZapIcon className='fill-white ml-2' />
+      </Button>
     </div>
   );
 };

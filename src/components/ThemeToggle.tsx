7a61bbedaad87ff67cb21a37fee35a6a8e6a71e1
hopeVaughn <hopevaughnwarren@gmail.com>
2023-12-06 11:39:00 -0800

    update configs

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,6 +1,6 @@
 "use client";
-
 import * as React from "react";
+import { useState, useEffect } from "react";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
@@ -16,26 +16,37 @@ export function ThemeToggle({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
-  const { setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const isThemeActive = (value: string) => theme === value;
 
   return (
     <div className={className} {...props}>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="outline" size="icon">
-            <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <Sun className={`h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all ${isThemeActive('dark') ? '-rotate-90 scale-0' : ''}`} />
+            <Moon className={`absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all ${isThemeActive('dark') ? 'rotate-0 scale-100' : ''}`} />
             <span className="sr-only">Toggle theme</span>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-          <DropdownMenuItem onClick={() => setTheme("light")}>
+          <DropdownMenuItem onClick={() => setTheme("light")} className={isThemeActive("light") ? "active-theme" : ""}>
             Light
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("dark")}>
+          <DropdownMenuItem onClick={() => setTheme("dark")} className={isThemeActive("dark") ? "active-theme" : ""}>
             Dark
           </DropdownMenuItem>
-          <DropdownMenuItem onClick={() => setTheme("system")}>
+          <DropdownMenuItem onClick={() => setTheme("system")} className={isThemeActive("system") ? "active-theme" : ""}>
             System
           </DropdownMenuItem>
         </DropdownMenuContent>

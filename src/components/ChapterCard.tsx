5f28e7b34e672de92fbb4511117d0b6bc3b04d4c
hopeVaughn <hopevaughnwarren@gmail.com>
2023-12-14 10:21:53 -0800

    update chapterCard to forward refs to activate triggerload function

--- src/components/ChapterCard.tsx
+++ src/components/ChapterCard.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useState, useRef, useEffect, useCallback, forwardRef } from 'react';
 import axios from 'axios';
 import { Chapter } from '@prisma/client';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
@@ -27,165 +27,156 @@ type EditChapterPayload = {
   };
 };
 
+const ChapterCard = forwardRef<ChapterCardHandler, Props>(
+  ({ chapter, chapterIndex, unitId, completedChapterIds, setCompletedChapterIds }, ref) => {
+    const { toast } = useToast();
+    const [isEditMode, setIsEditMode] = useState(false);
+    const chapterNameRef = useRef<HTMLInputElement>(null);
+    const queryClient = useQueryClient();
+    const { updateChapter } = useCourseStore((state) => state);
 
-const ChapterCard = React.forwardRef<ChapterCardHandler, Props>(({ chapter, chapterIndex, unitId, completedChapterIds, setCompletedChapterIds }, ref) => {
-  const { toast } = useToast();
-  const [isEditMode, setIsEditMode] = React.useState(false);
-  const chapterNameRef = React.useRef<HTMLInputElement>(null);
-  const queryClient = useQueryClient();
-  const { updateChapter } = useCourseStore((state) => state);
+    // Mutation for editing chapter details
+    const { mutate: editChapterMutation } = useMutation({
+      mutationFn: async (updatedChapter: EditChapterPayload) => {
+        const response = await axios.patch('/api/chapter/editInfo', updatedChapter);
+        return response.data;
+      },
+      onError: (error: any) => {
+        toast({ title: 'Error updating chapter', description: error.message || 'Something went wrong', variant: 'destructive' });
+      }
+    });
 
-  // Mutation for editing chapter details
-  const { mutate: editChapterMutation } = useMutation({
-    mutationFn: async (updatedChapter: EditChapterPayload) => {
-      const response = await axios.patch('/api/chapter/editInfo', updatedChapter);
-      return response.data;
-    },
-    onError: (error: any) => {
-      toast({ title: 'Error updating chapter', description: error.message || 'Something went wrong', variant: 'destructive' });
-    }
-  });
+    const handleSave = () => {
+      if (chapterNameRef.current) {
+        const updatedChapterName = chapterNameRef.current.value;
+        editChapterMutation({
+          chapterId: chapter.id,
+          updatedData: {
+            name: updatedChapterName
+          }
+        }, {
+          onSuccess: () => {
+            updateChapter(unitId, chapter.id, { name: updatedChapterName });
+            queryClient.invalidateQueries({ queryKey: ['course'] });
+            toast({ title: 'Chapter updated successfully' });
+          }
+        });
+        setIsEditMode(false);
+      }
+    };
 
-
-  const handleSave = () => {
-    if (chapterNameRef.current) {
-      const updatedChapterName = chapterNameRef.current.value;
-      editChapterMutation({
-        chapterId: chapter.id,
-        updatedData: {
-          name: updatedChapterName
-        }
-      }, {
-        onSuccess: () => {
-          updateChapter(unitId, chapter.id, { name: updatedChapterName });
-          queryClient.invalidateQueries({ queryKey: ['course'] });
-          toast({ title: 'Chapter updated successfully' });
-        }
-      });
-      setIsEditMode(false);
-    }
-  };
-
-  const [success, setSuccess] = React.useState<boolean | null>(null);
-  const { mutate: getChapterInfo, isPending } = useMutation({
-    mutationFn: async () => {
-      try {
+    const [success, setSuccess] = useState<boolean | null>(null);
+    const { mutate: getChapterInfo, isPending } = useMutation({
+      mutationFn: async () => {
         const response = await axios.post('/api/chapter/getInfo', { chapterId: chapter.id });
         return response.data;
-      } catch (error) {
-        // Log detailed error
-        if (axios.isAxiosError(error)) {
-          console.error('Axios Error in call from ChapterCard:', error.response?.data || error.message);
-        } else {
-          console.error('Unexpected Error:', error);
-        }
-        throw error;
-      }
-    },
-    onSuccess: () => {
-      setSuccess(true);
-      addChapterIdToSet();
-    },
-    onError: (error) => {
-      setSuccess(false);
-      toast({
-        title: error.message || 'Error loading chapter info in ChapterCard',
-        description: 'Failed to load chapter info',
-      });
-      addChapterIdToSet();
-    }
-  });
-
-  const addChapterIdToSet = React.useCallback(() => {
-    setCompletedChapterIds((prev) => {
-      const newSet = new Set(prev);
-      newSet.add(chapter.id);
-      return newSet;
-    });
-  }, [chapter.id, setCompletedChapterIds]);
-
-  React.useEffect(() => {
-    if (chapter.videoId) {
-      setSuccess(true);
-      addChapterIdToSet();
-    }
-  }, [chapter, addChapterIdToSet]);
-  React.useImperativeHandle(ref, () => ({
-    async triggerLoad() {
-      if (chapter.videoId) {
+      },
+      onSuccess: () => {
+        setSuccess(true);
         addChapterIdToSet();
-        return;
+      },
+      onError: (error) => {
+        setSuccess(false);
+        toast({
+          title: error.message || 'Error loading chapter info in ChapterCard',
+          description: 'Failed to load chapter info',
+        });
+        addChapterIdToSet();
       }
-      getChapterInfo(undefined, {
-        onSuccess: () => {
-          setSuccess(true);
-          addChapterIdToSet();
-        },
-        onError: (error) => {
-          setSuccess(false);
-          toast({
-            title: 'Error',
-            description: 'Failed to load chapter info',
-            variant: 'destructive',
-          });
-          addChapterIdToSet();
-        }
+    });
+
+    const addChapterIdToSet = useCallback(() => {
+      setCompletedChapterIds((prev) => {
+        const newSet = new Set(prev);
+        newSet.add(chapter.id);
+        return newSet;
       });
-    }
-  }));
+    }, [chapter.id, setCompletedChapterIds]);
 
-  const handleEdit = () => {
-    setIsEditMode(true);
-  };
-
-  const handleCancel = () => {
-    setIsEditMode(false);
-  };
-
-  return (
-    <div className={cn(
-      'px-4 py-2 mt-2 rounded flex items-center justify-between',
-      {
-        'bg-secondary': success === null,
-        'bg-red-500': success === false,
-        'bg-green-500': success === true,
+    useEffect(() => {
+      if (chapter.videoId) {
+        setSuccess(true);
+        addChapterIdToSet();
       }
-    )}>
-      {isEditMode ? (
-        <>
-          <input
-            ref={chapterNameRef}
-            defaultValue={chapter.name}
-            className="flex-1 px-2 py-1 border border-gray-300 rounded"
-          />
-          <button
-            onClick={handleSave}
-            className="ml-2 p-1 text-green-600 hover:text-green-800"
-          >
-            <SaveAll />
-          </button>
-          <button
-            onClick={handleCancel}
-            className="ml-2 p-1 text-red-600 hover:text-red-800"
-          >
-            <XIcon />
-          </button>
-        </>
-      ) : (
-        <>
-          <h5 className="flex-1">Chapter {chapterIndex + 1}: {chapter.name}</h5>
-          <button
-            onClick={handleEdit}
-            className="p-1 text-blue-600 hover:text-blue-800"
-          >
-            <Edit />
-          </button>
-        </>
-      )}
-      {isPending && <Loader2 className='animate-spin' />}
-    </div>
-  );
-});
+    }, [chapter, addChapterIdToSet]);
+
+    React.useImperativeHandle(ref, () => ({
+      triggerLoad() {
+        console.log('triggerLoad called for chapter', chapter.id);
+        if (chapter.videoId) {
+          addChapterIdToSet();
+          return;
+        }
+        getChapterInfo(undefined, {
+          onSuccess: () => {
+            setSuccess(true);
+            addChapterIdToSet();
+          },
+          onError: (error) => {
+            setSuccess(false);
+            toast({
+              title: 'Error',
+              description: 'Failed to load chapter info',
+              variant: 'destructive',
+            });
+            addChapterIdToSet();
+          }
+        });
+      }
+    }));
+
+    const handleEdit = () => {
+      setIsEditMode(true);
+    };
+
+    const handleCancel = () => {
+      setIsEditMode(false);
+    };
+
+    return (
+      <div className={cn(
+        'px-4 py-2 mt-2 rounded flex items-center justify-between',
+        {
+          'bg-secondary': success === null,
+          'bg-red-500': success === false,
+          'bg-green-500': success === true,
+        }
+      )}>
+        {isEditMode ? (
+          <>
+            <input
+              ref={chapterNameRef}
+              defaultValue={chapter.name}
+              className="flex-1 px-2 py-1 border border-gray-300 rounded"
+            />
+            <button
+              onClick={handleSave}
+              className="ml-2 p-1 text-green-600 hover:text-green-800"
+            >
+              <SaveAll />
+            </button>
+            <button
+              onClick={handleCancel}
+              className="ml-2 p-1 text-red-600 hover:text-red-800"
+            >
+              <XIcon />
+            </button>
+          </>
+        ) : (
+          <>
+            <h5 className="flex-1">Chapter {chapterIndex + 1}: {chapter.name}</h5>
+            <button
+              onClick={handleEdit}
+              className="p-1 text-blue-600 hover:text-blue-800"
+            >
+              <Edit />
+            </button>
+          </>
+        )}
+        {isPending && <Loader2 className='animate-spin' />}
+      </div>
+    );
+  });
 
 ChapterCard.displayName = 'ChapterCard';
 export default ChapterCard;
--- src/components/ConfirmChapters.tsx
+++ src/components/ConfirmChapters.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Link from 'next/link';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
@@ -14,58 +14,33 @@ type Props = {
   courseId: string;
 };
 
-type ChapterRefs = Record<string, React.RefObject<ChapterCardHandler>>;
-
 const ConfirmChapters = ({ courseId }: Props) => {
-  const [loading, setLoading] = React.useState(false);
-  const [completedChapterIds, setCompletedChapterIds] = React.useState<Set<String>>(new Set());
+  const [loading, setLoading] = useState(false);
+  const [completedChapterIds, setCompletedChapterIds] = useState<Set<String>>(new Set());
   const { course: storedCourse, setCourse } = useCourseStore();
+  const chapterRefs = useRef<Record<string, React.RefObject<ChapterCardHandler>>>({});
 
-  // Use `useQuery` to fetch course data
-  const {
-    data: courseData,
-    isPending,
-    isSuccess,
-    isError,
-    error } = useQuery<ExtendedCourse, Error>({
-      queryKey: ['course', courseId],
-      queryFn: () => axios.get(`/api/course/getCourse?courseId=${courseId}`).then(res => res.data.course),
-    },
-    );
+  const { data: courseData, isPending, isSuccess, isError, error } = useQuery<ExtendedCourse, Error>({
+    queryKey: ['course', courseId],
+    queryFn: () => axios.get(`/api/course/getCourse?courseId=${courseId}`).then(res => res.data.course),
+  });
+
   const course = courseData;
-  const chapterRefs = React.useRef<ChapterRefs>({});
-  console.log('courseData FROM CONFIRMCHAPTERS:', courseData);
-
-  React.useEffect(() => {
-    if (course) {
-      course.units.forEach((unit) => {
-        unit.chapters.forEach((chapter) => {
-          const chapterIdKey = String(chapter.id);
-          if (!chapterRefs.current[chapterIdKey]) {
-            chapterRefs.current[chapterIdKey] = React.createRef();
-          }
-        });
-      });
-    }
-  }, [course]);
-
 
   const totalChaptersCount = React.useMemo(() => {
     if (!course || !course.units) {
-      return 0; // Return 0 if course or course.units is undefined
+      return 0;
     }
     return course.units.reduce((acc, unit) => acc + unit.chapters.length, 0);
-  }, [course?.units]); // Depend on course.units
-
+  }, [course?.units]);
 
   if (isPending) {
-    return <CardSkeleton />; // Show loading state
+    return <CardSkeleton />;
   }
 
   if (isError) {
-    return <div>Error: {error?.message}</div>; // Show error state
+    return <div>Error: {error?.message}</div>;
   }
-
 
   return (
     <div className='w-full mt-4'>
@@ -78,13 +53,17 @@ const ConfirmChapters = ({ courseId }: Props) => {
             <h3 className='text-2xl font-bold'>{unit.name}</h3>
             <div className="mt-3">
               {unit.chapters.map((chapter, chapterIndex) => {
+                const chapterIdKey = String(chapter.id);
+                if (!chapterRefs.current[chapterIdKey]) {
+                  chapterRefs.current[chapterIdKey] = React.createRef();
+                }
                 return (
                   <ChapterCard
+                    ref={chapterRefs.current[chapterIdKey]}
+                    key={chapter.id}
                     completedChapterIds={completedChapterIds}
                     setCompletedChapterIds={setCompletedChapterIds}
-                    ref={chapterRefs.current[String(chapter.id)]}
                     unitId={unit.id}
-                    key={chapter.id}
                     chapter={chapter}
                     chapterIndex={chapterIndex}
                   />
@@ -97,39 +76,31 @@ const ConfirmChapters = ({ courseId }: Props) => {
       <div className="flex items-center justify-center mt-4">
         <Separator className='flex-[1]' />
         <div className="flex items-center mx-4">
-          <Link href='create' className={buttonVariants({
-            variant: 'secondary',
-          })}>
+          <Link href='create' className={buttonVariants({ variant: 'secondary' })}>
             <ChevronLeft className='w-4 h-4 mr-2' strokeWidth={4} />
-            Back</Link>
-          {
-            totalChaptersCount === completedChapterIds.size ? (
-              <Link
-                className={buttonVariants({
-                  className: "ml-4 font-semibold",
-                })}
-                href={`/course/${course!.id}/0/0`}
-              >Save & Continue
-                <ChevronRight className='w-4 h-4 ml-2' />
-              </Link>
-            ) : (
-              <Button
-                type="button"
-                className="ml-4 font-semibold"
-                disabled={loading}
-                onClick={() => {
-                  setLoading(true);
-                  Object.values(chapterRefs).forEach((ref) => {
-                    ref.current?.triggerLoad();
-                  });
-                }}
-              >
-                Generate
-                <ChevronRight className="w-4 h-4 ml-2" strokeWidth={4} />
-              </Button>
-            )
-          }
-
+            Back
+          </Link>
+          {totalChaptersCount === completedChapterIds.size ? (
+            <Link className={buttonVariants({ className: "ml-4 font-semibold" })} href={`/course/${course!.id}/0/0`}>
+              Save & Continue
+              <ChevronRight className='w-4 h-4 ml-2' />
+            </Link>
+          ) : (
+            <Button
+              type="button"
+              className="ml-4 font-semibold"
+              disabled={loading}
+              onClick={() => {
+                setLoading(true);
+                Object.values(chapterRefs.current).forEach((ref) => {
+                  ref.current?.triggerLoad();
+                });
+              }}
+            >
+              Generate
+              <ChevronRight className="w-4 h-4 ml-2" strokeWidth={4} />
+            </Button>
+          )}
         </div>
         <Separator className='flex-[1]' />
       </div>
